c882d84b9fda88f395a16821fe811a88edb77bd3
mohit01717 <mohitgagneja943@gmail.com>
2024-02-24 12:17:09 +0530

    changes in login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,44 +2,25 @@ import React, { useState } from "react";
 import EyeOn from "remixicon-react/EyeLineIcon";
 import OffEye from "remixicon-react/EyeOffLineIcon";
 import Stop_circle from "remixicon-react/StopCircleLineIcon";
+import { useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
 import "./login.css";
-const Input = ({ type, name, placeholder, onchange, value }) => {
+
+const Button = ({ name, onclick }) => {
   return (
-    <input
-      className="Input_user"
-      type={type}
-      name={name}
-      placeholder={placeholder}
-      onChange={onchange}
-      value={value}
-      required
-    />
+    <button className="btn" onClick={onclick}>
+      {name}
+    </button>
   );
 };
-const Button = ({ name,onclick }) => {
-  return <button className="btn" onClick={onclick}>{name}</button>;
-};
 const Login = ({ name }) => {
-  const [email, setemail] = useState("");
-  const [password, setpassword] = useState("");
-  const [data, setdata] = useState({});
   const [eye, seteye] = useState(false);
   const [stop, setstop] = useState(false);
-
-function handleClick(){
-    // Perform validation
-    const newErrors = {};
-    if (email.trim() === '') {
-     
-      newErrors.username = 'Username is required';
-    }
-    else{
-      
-    }
-    if (password.trim() === '') {
-      newErrors.password = 'Password is required';
-    }
-}
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
 
   return (
     <div className="Login-outer-container">
@@ -52,36 +33,66 @@ function handleClick(){
       </div>
       <div className="login_container">
         <h1>{name}</h1>
-        <div className="login_input_border">
-          <Input
-            type={"email"}
-            name={"email"}
-            placeholder={"Email"}
-            onchange={(e) => setemail(e.target.value)}
-            value={email}
-          />
-          {stop ? <Stop_circle color="red" /> : <Stop_circle color="black" />}
-        </div>
-        <div className="login_input_border">
-          <Input
-            type={eye?"text":"password"}
-            name={"password"}
-            
-            placeholder={"Password"}
-            onchange={(e) => setpassword(e.target.value)}
-            value={password}
-          />
-          {eye ? (
-            <EyeOn
-              style={{ cursor: "pointer" }}
-              onClick={() => seteye(false)}
+        <form
+          onSubmit={handleSubmit((data) => {
+            console.log(data);
+          })}
+        >
+          <div className="login_inside_container">
+            <div className="login_input_border">
+              <input
+                {...register("email", { required: "Email is required" })}
+                type={"email"}
+                name={"email"}
+                placeholder={"Email"}
+                className="Input_user"
+              />
+              {stop ? (
+                <Stop_circle color="red" />
+              ) : (
+                <Stop_circle color="black" />
+              )}
+            </div>
+            <ErrorMessage
+              errors={errors}
+              name="email"
+              render={({ message }) => (
+                <p style={{ color: "red" }}>{message}</p>
+              
+              )}
             />
-          ) : (
-            <OffEye style={{cursor:"pointer"}} onClick={() => seteye(true)} />
-          )}
-        </div>
+            <div className="login_input_border">
+              <input
+                {...register("password", { required: "Password is required" })}
+                type={eye ? "text" : "password"}
+                name={"password"}
+                placeholder={"Password"}
+                className="Input_user"
+              />
+              {eye ? (
+                <EyeOn
+                  style={{ cursor: "pointer" }}
+                  onClick={() => seteye(false)}
+                />
+              ) : (
+                <OffEye
+                  style={{ cursor: "pointer" }}
+                  onClick={() => seteye(true)}
+                />
+              )}
+            </div>
+          
 
-        <Button name={name} onclick={handleClick}  />
+            <ErrorMessage
+              errors={errors}
+              name="password"
+              render={({ message }) => (
+                <p style={{ color: "red" }}>{message}</p>
+              )}
+            />
+            <Button name={name} />
+          </div>
+        </form>
       </div>
     </div>
   );
